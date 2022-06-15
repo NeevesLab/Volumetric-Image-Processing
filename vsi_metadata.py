8ada315e32417d61293ae2fff089ae54f027ec05
NeevesLab <75540337+NeevesLab@users.noreply.github.com>
2022-06-15 17:40:09 -0600

    Update vsi_metadata.py

--- vsi_metadata.py
+++ vsi_metadata.py
@@ -1,40 +1,23 @@
 import javabridge
 import xml.etree.ElementTree as ET
 import bioformats
-
-default_locations=[['Loop','cycle time'],['Loop','Stage loop','Z-Stack','relative step width']]
-default_tag=[['node','attribute'],['node','node','node','attribute']]
+import re
 
 # ---- Main function that extracts the relevant metadata from a vsi file
-def extract_metadata(filepath,cycle_vm=True,meta_number=None,stage_loop=True):
+def extract_metadata(filepath,cycle_vm=True,meta_number=None):
     if cycle_vm:
         javabridge.start_vm(class_path=bioformats.JARS)
     biof=extract_meta_bioformats(filepath)
     if meta_number is not None:
         filepath=change_file_num(filepath,meta_number)
-    metadata=extract_meta_manual(filepath,metadata=biof,stage_loop=stage_loop)
+    metadata=extract_meta_manual(filepath,metadata=biof)
     if cycle_vm:
         javabridge.kill_vm()
     return metadata
 
 def change_file_num(string,meta_number):
-    nums=[]
-    location=[]
-    split_string=split(string)
-    for i in range(len(split_string)):
-        if split_string[i].isdigit():
-            nums.append(split_string[i])
-            location.append(i)
-    nums=[int(i) for i in split(string) if i.isdigit()]
-    original=nums[-1]
-    update=meta_number
-    loc=location[-1]
-    split_string[loc]=str(update)
-    new_string = "".join(split_string)
+    new_string = re.sub('(\d+)(?!.*\d)',str(meta_number),string)
     return new_string
-
-def split(word):
-    return [char for char in word]
 
 # ---- Function that gets the attainable information using bioformats
 def extract_meta_bioformats(filepath, metadata=dict()):
@@ -49,47 +32,46 @@ def extract_meta_bioformats(filepath, metadata=dict()):
 # ---- Function that manually reads through oex metadata file and gets other relevant information
 #      paths through the xml file to final metadata value
 
-def extract_meta_manual(file_path,locations=default_locations,tag=default_tag,metadata=dict(),stage_loop=True):
+def extract_meta_manual(file_path,tag=default_tag,metadata=dict(),keys=['cycle time','cycle time_unit','relative step width']):
     file_path=file_path.replace('vsi','oex')
-    
-    if stage_loop == False:
-        locations=[['Loop','cycle time'],
-                           ['Loop','Z-Stack','relative step width']]
-        tag=[['node','attribute'],['node','node','attribute']]
+    # read in file as xml tree
     tree=ET.parse(file_path)
     root=tree.getroot()
-    # get into net
-    for subroot in root:
-        if subroot.tag=='net':
-            root=subroot
-            break
-    # iterate through the different values we want to get
-    for i in range(len(locations)):
-        loop_root=root
-        path=locations[i][:]
-        path_tag=tag[i][:]
-        # naviate through the directed paths of the xml file
-        for j in range(len(path)):
-            loop_root=query_branches(loop_root,path_tag[j],path[j])
-            # if at the end of the path get the metadata value we want and append it to a dictionary
-            if j==len(path)-1:
-                if  loop_root!='not_found':
-                    for l in loop_root:
-                        metadata[path[j]]=l.get('val')
-                        
+    # convert tree to string and split lines
+    xmlstr=ET.tostring(root, encoding='utf8', method='xml').decode()
+    split=xmlstr.splitlines()
+    # find the line location of ketys
+    line_location=[]
+    for k in keys:
+        count=0
+        # here we need to add quotation marks to match the style of the xml file
+        modified_key='"{}"'.format(k)
+        # if a line contains our key we store the next line number in a list, as 
+        # this is where the value is contained
+        for line in split:
+            if modified_key in line:
+                line_location.append(count+1)
+                break
+            count+=1
+     # iterate through the line number values and extract the numbers from them and
+     # compile to a list 
+    values=[]
+    for l in line_location:
+        print(split[l])
+        numeric_const_pattern = '[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
+        rx = re.compile(numeric_const_pattern, re.VERBOSE)
+        digit=float(rx.findall(split[l])[0])
+        values.append(digit)
+    # store manual_metadata values in a dictionary
+    manual_metadata=dict(zip(keys,values))
+    # convert cycle time to seconds
+    if manual_metadata['cycle time_unit']==1: # unit of 1 corresponds to hours
+        manual_metadata['cycle time']=manual_metadata['cycle time']/3600
+    elif manual_metadata['cycle time_unit']==2:# unit of 2 corresponds to minutes
+        manual_metadata['cycle time']=manual_metadata['cycle time']/60
+    elif manual_metadata['cycle time_unit']==4:# unit of 4 corresponds to milliseconds
+        manual_metadata['cycle time']=manual_metadata['cycle time']/60
+    # and we skip the unit of 3 as it corresponds to seconds
+    manual_metadata.pop('cycle time_unit')
+    metadata=metadata|manual_metadata
     return metadata
-# ---- Function to look in a directory of the xml and find the subdirectory you're querying. Used for the manual
-#      metedata extraction
-def query_branches(loop,tag,attrib):
-    found=False
-    # search through subdirectories of directory
-    for l in loop:
-        # if the tag and attribute match of subdir match return the subdir
-        if l.tag ==tag and l.attrib['name']==attrib:
-            new_loop=l
-            found=True
-            break
-    if found:
-        return new_loop
-    else:
-        return 'not_found'
